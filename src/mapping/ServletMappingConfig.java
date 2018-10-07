a2ac6869aa705e62231926e81cd54e1c0aa8dad1
Rudbeckia.hirta.L <83773602@qq.com>
2018-10-07 12:47:27 +0800

    Update src/mapping/ServletMappingConfig.java

--- src/mapping/ServletMappingConfig.java
+++ src/mapping/ServletMappingConfig.java
@@ -7,6 +7,7 @@ import java.util.List;
  * 表现了 url 与 servlet 的映射关系
  *
  * 实际中这个类应该与 web.xml 相关联
+ * 在web.xml中通过<servlet></servlet>和<servlet-mapping></servlet-mapping>来进行指定哪个URL交给哪个servlet进行处理
  * */
 
 public class ServletMappingConfig {
